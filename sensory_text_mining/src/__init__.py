e72295c374cc091017f0d2bbfd2e358c83d2f88d
SZ.Song <14408924+SEM_mim@user.noreply.gitee.com>
2026-01-30 16:20:04 +0800

    pie

--- sensory_text_mining/src/__init__.py
+++ sensory_text_mining/src/__init__.py
@@ -3,3 +3,22 @@ from wordcloud import freq_dict_wordcloud
 from utils import *
 from color_map import color_map
 from radar import plot_sensory_radar_chart
+from category import classify_product, count_product_categories, calculate_category_percentage, plot_product_category_pie_chart
+
+__all__ = [
+    'product_sensory_adj_token_count',
+    'province_sensory_adj_tokens',
+    'SENSORY_DIMENSIONS',
+    'classify_sensory_dimension',
+    'count_province_sensory_dimensions',
+    'normalize_dimension_counts',
+    'select_top_provinces',
+    'freq_dict_wordcloud',
+    'counter_most_common_list_to_dict',
+    'color_map',
+    'plot_sensory_radar_chart',
+    'classify_product',
+    'count_product_categories',
+    'calculate_category_percentage',
+    'plot_product_category_pie_chart',
+]
